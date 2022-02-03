f639a80ddd9a63723c9c55c51a7ba0147c40511a
MariaJesus <mjmarugan_@hotmail.com>
2022-02-03 13:09:01 +0100

    Ejercicio 4 M5_L2_U2

---
+++ M5_L2_U2/Ejercicio 4 M5_L2_U2.js
@@ -0,0 +1,22 @@
+//Completar las condiciones de los IF para que los mensajes
+//se muestren de forma correcta
+
+var numero1=5;
+const num2=8; 
+const num1=5;
+if (num1<num2){
+    //alert (num1 +" no es mayor que "+ num2);
+}
+
+if (num2>0){
+    //alert (num2 +" es positivo ");
+}
+
+if (num1<0 || num1 !=0){
+    //alert (num1 +" es negativo o distinto de 0 ");
+}
+
+++numero1;
+if (numero1 <= num2){
+    alert ("Incrementar en 1 el valor de " + num1 + " no lo hace mayor o igual que " + num2);
+}
