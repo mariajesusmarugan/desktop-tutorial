c28374a80faa71df940706f8e120258604fdbf92
MariaJesus <mjmarugan_@hotmail.com>
2022-02-03 09:56:01 +0100

    Prueba Bucles

--- M5_L2_U2/Prueba Bucles.js
+++ M5_L2_U2/Prueba Bucles.js
@@ -6,12 +6,15 @@ for (let i= 0; i<10; i++){
 
 }
 
-//Prueba bucle for para arrays
+//Prueba bucle for para pintarala en consola, mejor coigo que console.log(i); anterior
 
 for (let i= 0; i<3; i++){
     console.log(`Esta es la iteración ${i}`);
 
 }
+
+
+//Podemos trabajar con arrays, devolviendonos las posiciones de éste
 
 var mesesAño = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
                 "Septiembre", "Octubre", "Noviembre", "Diciembre" ];
@@ -20,3 +23,23 @@ var mesesAño = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
         console.log(mesesAño[i]);
                 
      }
+
+//Podemos recorrer un array y obtener cada elemento FOR...OF
+
+var diasSemana = ["lunes", "martes","miercoles", "jueves","viernes", "sabado", "domingo"];
+    for (let i= 0; i<7; i++){
+    console.log(diasSemana[i]);
+            
+ }  
+    for (var elementos of diasSemana){
+    console.log(diasSemana);
+            
+ }
+
+//Podemos recorrer un array y obtener cada posicion FOR...IN
+
+var diasSemana = ["lunes", "martes","miercoles", "jueves","viernes", "sabado", "domingo"];
+    for (var posicion in diasSemana){
+    console.log(posicion);
+            
+ }
