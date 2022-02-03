96bd9e70fcbcae2cb29a5e79e6f47ce57b5cb596
MariaJesus <mjmarugan_@hotmail.com>
2022-02-03 09:31:42 +0100

    Prueba Bucles For

---
+++ M5_L2_U2/Prueba Bucles.js
@@ -0,0 +1,22 @@
+//Prueba bucle for condición simple
+
+for (let i= 0; i<10; i++){
+    console.log("dentro del bucle");
+    console.log(i);
+
+}
+
+//Prueba bucle for para arrays
+
+for (let i= 0; i<3; i++){
+    console.log(`Esta es la iteración ${i}`);
+
+}
+
+var mesesAño = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
+                "Septiembre", "Octubre", "Noviembre", "Diciembre" ];
+
+    for (let i= 0; i<12; i++){
+        console.log(mesesAño[i]);
+                
+     }
